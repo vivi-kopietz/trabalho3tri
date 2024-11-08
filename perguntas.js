d809295105c8e3450ecce5385a2ece0802416fb1
Viviane Kopietz <142537081+vivi-kopietz@users.noreply.github.com>
2024-11-07 21:19:41 -0300

    dd

---
+++ perguntas.js
@@ -0,0 +1,100 @@
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+
+)criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartão(
+    'Matematica',
+    'Como se calcula a soma dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
---
+++ página 2.js
@@ -0,0 +1,29 @@
+function criaCartão(categoria, pergunta, resposta){
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+    <div class="conteudo-cartao" onclick = "mostrarResposta(this)">
+       <h3>${categoria}</h3>
+       <div class="pergunta-cartao">
+       <p>${pergunta}</p>
+       </div>
+       <div class="resposta-cartao">
+           <p>${resposta}</p>
+       </div>
+   </div>
+    `
+    container.appendChild(cartao);
+   }
+
+   function mostrarResposta(card){
+    const resposta = card.querySelector('.resposta-cartao');
+
+    if(resposta.style.display === 'none'){
+resposta.style.display = 'block';
+    }
+    else{
+        resposta.style.display = 'none';
+    }
+
+   }
