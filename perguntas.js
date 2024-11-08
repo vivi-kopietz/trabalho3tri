8ac316222266ad069ec8dc1a6021b140a3ff599d
Viviane Kopietz <142537081+vivi-kopietz@users.noreply.github.com>
2024-11-08 08:37:30 -0300

    dfdf

--- perguntas.js
+++ perguntas.js
@@ -3,98 +3,117 @@ criaCartão(
     'Como se calcula a soma dos termos de uma P.A',
     'Utiliza-se a fórmula: ((a1+an)*r)/2'
 )
-criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
-)
-criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
-)
-criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
-)
-criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
-)
-criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
-)
-criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
-)
-criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
-)
-criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
-)
-criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
-)
-criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
-)
-criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
-)
-criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
-)
-criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
-)
-criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
-)
-criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
-)
-criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
 
-)criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
-)
 criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+    'História',
+    'Quanto tempo durou a Ditadura Civil-Militar no Brasil?',
+    '21 anos'
 )
+
 criaCartão(
-    'Matematica',
-    'Como se calcula a soma dos termos de uma P.A',
-    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+    'Português',
+    'O que é um verbo?',
+    'É uma classe gramatical que expressa ações, estados, fenômenos naturais, localizando-os no tempo'
+)
+
+criaCartão(
+    'Artes',
+    'que cor se obtem na mistura das cores azul e vermelho?',
+    'cor roxa ou violeta'
+)
+
+criaCartão(
+    'Biologia',
+    'Quantos ossos tem o corpo humano adulto? ',
+    '206 ossos'
+)
+
+criaCartão(
+    'Geografia',
+    'Quais são todos os continentes?',
+    'America, Europa, Africa, Asia, Antartica e Oceania'
+)
+
+criaCartão(
+    'Física',
+    'O que é MUV',
+    'Movimento Uniformemente Variado- é um movimento em que a velocidade varia de forma constante a cada segundo '
+)
+
+criaCartão(
+    'Química',
+    'O que são hidrocarbonetos',
+    'Compostos binários de Hidrogênio e Carbono'
+)
+
+criaCartão(
+    'Educação Financeira',
+    'Quanto de impostos o Brasil cobra por produtos importados',
+    'O Brasil cobra 20% para produtos abaixo de U$50, e acima 60%'
+)
+
+criaCartão(
+    'Empreendedorismo',
+    'O que é um perfil empreendedor?',
+    'É uma soma de atitudes e caracteristica que ajudam o profissional em sua trajetória'
+)
+
+criaCartão(
+    'Geografia',
+    'A Russia esta localizada na Ásia ou na Europa?',
+    ' Ambos os continentes'
+)
+
+criaCartão(
+    'História',
+    'Quais foram as principais causas da queda do Império Romano do Ocidente',
+    'Invasões barbáras, instabilidade política e econômica'
+)
+
+criaCartão(
+    'Filosofia',
+    'Qual o filosofo responsável pelo Mito da Caverna?',
+    'Platão'
+)
+
+criaCartão(
+    'Geografia',
+    'qual o menor país do mundo?',
+    'Vaticano'
+)
+
+criaCartão(
+    'Artes',
+    'Quem pintou a Monalisa',
+    'Leonardo da Vinci'
+)
+
+criaCartão(
+    'Programação',
+    'Como criar um link para o seu projeto?',
+    'importe seu projeto no aplicativo versel'
+)
+
+criaCartão(
+    'biologia',
+    'Quais são as fases do desenvolvimento embrionario?',
+    'Zigoto, Morula, blastocisto e glastrula'
+)
+
+criaCartão(
+    'Química',
+    'qual o prefixo correspondente a 4 carbonos?',
+    'But'
+)
+
+criaCartão(
+    'Educação Fisíca',
+    'Que ano foi criado o vôlei?',
+    '1895'
+)
+
+criaCartão(
+    'História',
+    'O que foi a KGB',
+    'Principal organização de serviços secretos da União Soviética'
 )
